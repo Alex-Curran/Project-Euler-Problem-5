d2749496b56ae3d7dcd93cc2d2409148f9d0cf96
Alex-Curran <alexcurran839@gmail.com>
2015-05-28 17:16:05 -0500

    Project Set-up

---
+++ Problem-5/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *   Project Euler: Problem 5
+ *   Alex Curran 
+ *   
+ *  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ *   
+ *  https://projecteuler.net/problem=5
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
